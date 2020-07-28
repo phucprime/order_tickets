95f02541c510dc27dd26d951994da0b08a11071b
Nguyen Hoang Phuc <40540902+phuccoder@users.noreply.github.com>
2020-07-28 17:04:03 +0700

    up to date

--- webpython/main.py
+++ webpython/main.py
@@ -2,9 +2,29 @@ from webpython import app
 from flask import render_template
 
 
-@app.route('/')
-def admin():
+@app.route('/admin')
+def admin_index():
     return render_template("admin/index.html")
+
+
+@app.route('/admin/internal-flight')
+def flight_index_internal():
+    return render_template("admin/flight-internal.html")
+
+
+@app.route('/admin/external-flight')
+def flight_index_external():
+    return render_template("admin/flight-external.html")
+
+
+@app.route("/admin/order")
+def order_index():
+    return render_template("admin/order.html")
+
+
+@app.route("/admin/report")
+def report_index():
+    return render_template("admin/report.html")
 
 
 if __name__ == '__main__':
